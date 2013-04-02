5e720a44bf05c2c1f10adb34409f277059c213c0
Shane Liu <shane7226107@gmail.com>
2013-04-03 01:24:35 +0800

    single state crf ok

--- temporalPyramid.cpp
+++ temporalPyramid.cpp
@@ -8,22 +8,6 @@
 
 #include "temporalPyramid.h"
 
-vector<string> split(string& str,const char* c)
-{
-    char *cstr, *p;
-    vector<string> res;
-    cstr = new char[str.size()+1];
-    strcpy(cstr,str.c_str());
-    p = strtok(cstr,c);
-    while(p!=NULL)
-    {
-        res.push_back(p);
-        p = strtok(NULL,c);
-    }
-    return res;
-}
-
-
 TemporalPyramid::TemporalPyramid(){
 }
 
