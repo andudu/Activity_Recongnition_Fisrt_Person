c6d35e726a256d15b0711dcce727f832cb6b479e
Shane Liu <shane7226107@gmail.com>
2013-04-17 03:46:26 +0800

    ya

--- run.sh
+++ run.sh
@@ -1,38 +1,47 @@
 echo "run by shell"
 
-index="02"
+index="13"
 video="/Users/hmliu/Documents/CMLab/Master/ADL_code/ADLdataset/ADL_videos/split_frames_P_${index}"
 annotation="translated_with_obj_name/object_annot_P_${index}_translated_with_obj_name.txt"
 #crf_model_path="crf/multi_stage/5_fold/model_fold_1.crf"
 #crf_model_path="crf/multi_stage/1_vs_all/model_13.crf"
 crf_model_path="crf/multi_stage/1_vs_all_more_segment_2/model_${index}.crf"
 
-# Good results
 # Watching TV in P02
-#start="69235"
+# start="69235"
+# length="1000"
 
-# using computer in  P02
+# using computer in  P02 (simple and boring)
 # start="74335"
+# length="600"
 
 # Brushing teeth in P02
 # A temporal pyramid demo 
-start="500"
-length="1200"
+# start="500"
+# length="1200"
 
 # making tea stage 1 in P13 
 # start="500"
 # making tea stage 2 in P13 
-# start="21810"
+#start="21810"
+# length="900"
 
-# A temporal pyramid demo P20 
+# A temporal pyramid demo P20 washing dishes
 # start="13000"
-# length="1000"
+# length="900"
 
+# making cold food snack in P10
+# start="1000"
+# length="2000"
+
+# A 3 activities demo in P13
+start="42800"
+length="2000"
 
 #indicate="-indicate 2"
 show="-show"
 show_pyramid="-show_pyramid"
-#build_pyramid="-build_pyramid"
+build_pyramid="-build_pyramid"
 activity_prediction="-activity_prediction"
 #pause="-pause"
 ground_truth_detect="-ground_truth"
