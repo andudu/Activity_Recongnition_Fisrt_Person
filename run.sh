2533bd570ba5a25d84c802c10ed932e12bd9ec15
Shane Liu <shane7226107@gmail.com>
2013-06-04 11:09:43 +0800

    demo version

--- run.sh
+++ run.sh
@@ -1,10 +1,10 @@
 echo "run by shell"
 
-i="2"
+i="20"
 
-#crf_model_path="crf/multi_stage/5_fold/model_fold_1.crf"
-#crf_model_path="crf/multi_stage/1_vs_all/model_13.crf"
-crf_model_path="crf/multi_stage/1_vs_all_more_segment/model_${i}.crf"
+#crf_model_path="crf/multi_stage/1_vs_all_more_segment_2/model_${i}.crf"
+#crf_model_path="crf/multi_stage/1_vs_all_more_segment/model_${i}.crf"
+crf_model_path="crf/multi_stage/demo/model_${i}.crf"
 
 if [ $i -lt 10 ];         # If $i is smaller than 10
   then
@@ -29,8 +29,8 @@ length="600"
 
 # Brushing teeth in P02
 # A temporal pyramid demo 
-start="500"
-length="1200"
+# start="500"
+# length="1200"
 
 # making tea stage 1 in P13 
 # start="500"
@@ -40,12 +40,12 @@ length="1200"
 # length="900"
 
 # A temporal pyramid demo P20 washing dishes
-# start="13000"
-# length="800"
+start="13000"
+length="800"
 
 # making cold food snack in P10
-# start="1000"
-# length="2000"
+#start="1000"
+#length="2000"
 
 # A 3 activities demo in P13
 #start="42800"
@@ -54,13 +54,13 @@ length="1200"
 #indicate="-indicate 2"
 show="-show"
 show_pyramid="-show_pyramid"
-build_pyramid="-build_pyramid"
+#build_pyramid="-build_pyramid"
 activity_prediction="-activity_prediction"
 #pause="-pause"
 ground_truth_detect="-ground_truth"
 crf="-crf"
 FPN="-FPN 300"
-thres_factor="-thres_factor 300"
+thres_factor="-thres_factor 10"
 
 cmd="./FP_ADL.out -i ${video} -start ${start} -length ${length} -crf_model_path ${crf_model_path} -an ${annotation} ${show} ${pause} ${indicate} ${ground_truth_detect} ${crf} ${thres_factor} ${show_pyramid} ${build_pyramid} ${activity_prediction} ${FPN}"
 
