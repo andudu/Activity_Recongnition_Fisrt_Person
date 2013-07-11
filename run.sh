07d34b23ae813ab6307b1299c7a32c1a503ae055
hmliu <hmliu@ubuntu.(none)>
2013-07-11 15:29:07 +0800

    comments added in run.sh

--- run.sh
+++ run.sh
@@ -1,9 +1,9 @@
 echo "run by shell"
 
+#欲辨識影片代號
 i="13"
 
-#crf_model_path="crf/multi_stage/1_vs_all_more_segment_2/model_${i}.crf"
-#crf_model_path="crf/multi_stage/1_vs_all_more_segment/model_${i}.crf"
+#選擇CRF model(因為cross-validation,故每個video只對應到一個CRF model)
 crf_model_path="crf/multi_stage/demo/model_${i}.crf"
 
 if [ $i -lt 10 ];         # If $i is smaller than 10
@@ -13,12 +13,42 @@ if [ $i -lt 10 ];         # If $i is smaller than 10
     index=$i
 fi
 
+#影片拆解畫格的位置
 video="/Users/hmliu/Documents/CMLab/Master/ADL_code/ADLdataset/ADL_videos/split_frames_P_${index}"
+
+#Object annotation的位置
 annotation="translated_with_obj_name/object_annot_P_${index}_translated_with_obj_name.txt"
 
-start="1"
-length="600"
+#各影片長度(再run.sh裡沒有用到僅供參考)
+frame_numbers=(
+0
+53804
+85772
+71090
+47327
+44228
+46484
+79277
+47738
+38630
+28673
+14774
+25316
+53021
+45920
+47525
+25196
+26546
+34475
+113885
+48254
+)
 
+# 設定影片起點終點
+start="42800"
+length="1000"
+
+# 一些Good Cases
 # Watching TV in P02
 # start="69235"
 # length="1000"
@@ -48,21 +78,30 @@ length="600"
 #length="2000"
 
 # A 3 activities demo in P13
-start="42800"
-length="1600"
+# start="42800"
+# length="1600"
 
-#indicate="-indicate 2"
+# 操作選項,註解掉即為不啟用
+# 是否展示影片與偵物件測結果
 show="-show"
+# 是否在console印出temporal pyramid
 show_pyramid="-show_pyramid"
+# 是否建立temporal pyramid.不啟動的話pyramid只會有level 0,第一層
 build_pyramid="-build_pyramid"
-activity_prediction="-activity_prediction"
-#pause="-pause"
+# 當偵測到物件是否暫停影片畫面
+# pause="-pause"
+# 是否使用ground truth object annotation(就是本shell第20行指定的檔案)
 ground_truth_detect="-ground_truth"
+# 是否呼叫CRF++進行行為辨識
 crf="-crf"
+# 是否印出行為辨識結果
+activity_prediction="-activity_prediction"
+# Frame per segment in level 0
 FPN="-FPN 300"
+# 當物件存在一segemt中的次數>FPN/thres_factor, 視為本segment的feature
 thres_factor="-thres_factor 10"
 
-cmd="./FP_ADL.out -i ${video} -start ${start} -length ${length} -crf_model_path ${crf_model_path} -an ${annotation} ${show} ${pause} ${indicate} ${ground_truth_detect} ${crf} ${thres_factor} ${show_pyramid} ${build_pyramid} ${activity_prediction} ${FPN}"
+cmd="./FP_ADL.out -i ${video} -start ${start} -length ${length} -crf_model_path ${crf_model_path} -an ${annotation} ${show} ${pause} ${ground_truth_detect} ${crf} ${thres_factor} ${show_pyramid} ${build_pyramid} ${activity_prediction} ${FPN}"
 
 echo $cmd
 
